e1cdab96e315e878db416d08c642dc9c62088c08
Joe Molina <joedodaniljr123@gmail.com>
2024-04-05 16:06:43 -0400

    correcion de horrores ortograficos

--- src/app/auditoria/components/IniciosDeSesion.tsx
+++ src/app/auditoria/components/IniciosDeSesion.tsx
@@ -47,7 +47,7 @@ export function Auditoria() {
       <p className='mt-20 text-neutral-300'>Inicios de Sesion</p>
         <article className='my-2 bg-neutral-900 text-neutral-300 p-1 px-4 flex justify-start w-full '>
         <h3 className='w-32 mr-2'>Usuario</h3>
-        <p className=' w-60 mr-2 text-start'>Fecha y Hora de Coneccion</p>
+        <p className=' w-60 mr-2 text-start'>Fecha y Hora de Conexion</p>
         </article>
     <div className='flex flex-col overflow-auto '>
       {
